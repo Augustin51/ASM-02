0676922765bc877a7d2bfe4994175163bf7cba1e
Augustin51 <jacquesson.augustin@gmail.com>
2026-02-08 22:15:58 +0100

    add founction write_digit

--- index.s
+++ index.s
@@ -5,3 +5,15 @@ main:
     call write_digit
     ret
 
+write_digit:
+    push rbp
+    mov rbp, rsp
+
+    add rdi, 48
+    push rdi
+
+    mov rax, 1  
+    mov rdi, 1     
+    mov rsi, rsp   
+    mov rdx, 1     
+    syscall
